eccaf3ae2400ec2f04c015e638d59d98ab832653
holi <32414154@qq.com>
2017-02-11 15:33:23 +0800

    test junit is ready

---
+++ test/src/test/java/JUnitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by holi on 2/11/17.
+ */
+public class JUnitTest {
+    @Test
+    public void already() throws Exception {
+        assertTrue(true);
+    }
+}
